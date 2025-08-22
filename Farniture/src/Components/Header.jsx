93c0fb68200236f30f872c5e36ce74a7177f9b1e
kenenisa1 <kenenisamiesob@gmail.com>
2025-08-22 23:01:18 +0300

    Add styling to header component

--- Farniture/src/Components/Header.jsx
+++ Farniture/src/Components/Header.jsx
@@ -16,29 +16,29 @@ const Header = () => {
       <div className='container mx-auto px-4 py-3'>
         <div className='flex justify-between items-center'>
           <div className='flex items-center'>
-            <Link to='/' className='flex items-center'>
+            <Link to='/Home' className='flex items-center'>
               <img src={Logo} alt="Logo" className='h-10' />
               <h1 className='text-3xl px-2 font-bold text-amber-500'>Furni</h1>
             </Link>   
           </div>
           
           <nav className='hidden md:flex space-x-6'>
-            <Link to="#" className='hover:text-amber-600 px-3 py-2 text-black text-lg font-semibold transition-colors duration-200 border-b-2 border-transparent hover:border-amber-600'>
+            <Link to="/Home" className='hover:text-amber-600 px-3 py-2 text-black text-lg font-semibold transition-colors duration-200 border-b-2 border-transparent hover:border-amber-600'>
               Home
             </Link>
-            <Link to="#" className='hover:text-amber-600 px-3 py-2 text-black text-lg font-semibold transition-colors duration-200 border-b-2 border-transparent hover:border-amber-600'>
+            <Link to="./ShopHero" className='hover:text-amber-600 px-3 py-2 text-black text-lg font-semibold transition-colors duration-200 border-b-2 border-transparent hover:border-amber-600'>
               Shop
             </Link>
-            <Link to="#" className='hover:text-amber-600 px-3 py-2 text-black text-lg font-semibold transition-colors duration-200 border-b-2 border-transparent hover:border-amber-600'>
+            <Link to="/About" className='hover:text-amber-600 px-3 py-2 text-black text-lg font-semibold transition-colors duration-200 border-b-2 border-transparent hover:border-amber-600'>
               About
             </Link>
-            <Link to="#" className='hover:text-amber-600 px-3 py-2 text-black text-lg font-semibold transition-colors duration-200 border-b-2 border-transparent hover:border-amber-600'>
+            <Link to="/Contact" className='hover:text-amber-600 px-3 py-2 text-black text-lg font-semibold transition-colors duration-200 border-b-2 border-transparent hover:border-amber-600'>
               Contact
             </Link>
           </nav>
 
           <div className='hidden md:flex space-x-10'>
-            <Link to="#" className='text-gray-500 hover:text-amber-600 cursor-pointer transition-colors text-xl'>
+            <Link to="/Search" className='text-gray-500 hover:text-amber-600 cursor-pointer transition-colors text-xl'>
               <FiSearch/>
             </Link>
             <Link to="#" className='text-gray-500 hover:text-amber-600 cursor-pointer transition-colors text-xl'>
@@ -47,7 +47,7 @@ const Header = () => {
             <Link to="#" className='text-gray-500 hover:text-amber-600 cursor-pointer transition-colors text-xl'>
               <FiHeart/>
             </Link>
-            <Link to="#" className='text-gray-500 hover:text-amber-600 cursor-pointer transition-colors text-xl'>
+            <Link to="/Cart" className='text-gray-500 hover:text-amber-600 cursor-pointer transition-colors text-xl'>
               <FiShoppingCart/>
             </Link>
           </div>
@@ -57,7 +57,7 @@ const Header = () => {
               onClick={toggleMenu}
               className='text-gray-500 hover:text-amber-600 focus:outline-none text-2xl'
             >
-              {isMenuOpen ? <FiX className='cursor-pointer'/> : <FiMenu />}
+              {isMenuOpen ? <FiX className='cursor-pointer'/> : <FiMenu className='cursor-pointer'/>}
             </button>
           </div>
         </div>
