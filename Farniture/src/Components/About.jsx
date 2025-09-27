abdf9e99f39ff054d11c477ae36d3e43b95a3cc3
kenenisa1 <kenenisamiesob@gmail.com>
2025-09-27 23:16:44 +0300

    Refactor (About): Remove blog link for code simplicity

--- Farniture/src/Components/About.jsx
+++ Farniture/src/Components/About.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
 import Logo from '../assets/logo.png'
 import Hero from './Hero'
 import Features from './Features'
-import { Link } from 'react-router-dom'
+import easyShopping from '../assets/easyShopping.jpg'
 
 const About = () => {
+
   return (
     <div>
         <Hero PageName="About" img={Logo}/>
-        <Link to='/Blog' className='text-amber-600 font-bold text-lg hover:underline'>
-          Blog
-        </Link>
+
+        <div className='items-center justify-center gap-3 flex flex-col lg:flex-row xl:flex-row'>
+
+          <div className='text-left '>
+            <h1 className='text-5xl text-center '>Easy Shopping!</h1>
+          </div>
+
+          <div>
+            <img src={easyShopping} alt="" />
+          </div>
+
+        </div>
         <Features/>
     </div>
   )
